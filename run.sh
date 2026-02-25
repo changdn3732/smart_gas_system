e2b1f77e03bb96c56bd76be3ea32151b06b5abe6
changdn3732 <zixaid@gmail.com>
2026-02-25 23:25:44 +0900

    Add run.sh

--- run.sh
+++ run.sh
@@ -3,17 +3,13 @@
 APP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$APP_DIR"
 
-# 가상환경 없으면 생성
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
     python3 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+else
+    source venv/bin/activate
 fi
 
-source venv/bin/activate
-
-# 패키지 없으면 설치
-pip install --upgrade pip
-pip install -r requirements.txt
-
-# 앱 실행
 python main.py
